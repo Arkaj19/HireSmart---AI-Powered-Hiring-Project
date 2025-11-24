b712f877f9a39ba3a872e065183cb3b30ae07eb5
Arkaj19 <arkajyotikarmakar56@gmail.com>
2025-11-24 20:24:44 +0530

    Added render link

--- backend/main.py
+++ backend/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://hiresmart-ai-powered-hiring-project.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
